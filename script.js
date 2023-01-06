06636b136395868830c8b3989e2985e3fed1b687
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2023-01-06 12:55:45 +0100

    Add restart button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var timer = document.getElementById("timer");
 var word = document.getElementById("word");
+var hint = document.getElementById("hint");
 var holder = document.getElementById("holder");
 var languageSelector = document.getElementById("language-selector");
 var game = document.getElementById("game");
+
+const score = document.querySelector('.score span')
 
 var points = 0
 var seconds = 60
@@ -12,50 +15,83 @@ var wordsArrayBosnian  = [  'jabuka',  'banana',  'naranča',  'jagoda',  'grož
 
 var wordsArray = wordsArrayEnglish;
 
+var wordsArray = wordsArrayEnglish;
+
 document.getElementById('english-button').addEventListener('click', function() {
-    wordsArray = wordsArrayEnglish;
-    languageSelector.style.display = 'none';
-    game.style.display = 'block';
-    updateWord();
-    countDown();
-  });
+  wordsArray = wordsArrayEnglish;
+  languageSelector.style.display = 'none';
+  game.style.display = 'block';
+  updateWord();
+  countDown();
+  hint.innerHTML = "Press ENTER to confirm word"
+});
 
-  document.getElementById('bosnian-button').addEventListener('click', function() {
-    wordsArray = wordsArrayBosnian;
-    languageSelector.style.display = 'none';
-    game.style.display = 'block';
-    updateWord();
-    countDown();
-  });
+document.getElementById('bosnian-button').addEventListener('click', function() {
+  wordsArray = wordsArrayBosnian;
+  languageSelector.style.display = 'none';
+  game.style.display = 'block';
+  updateWord();
+  countDown();
+  hint.innerHTML = "Pritisni ENTER da potvrdis rijec"
+});
 
-  function updateWord() {
-    wordsArray.sort(function(){return 0.5 - Math.random()});
-    word.innerHTML = wordsArray[0];
+holder.onkeyup = function() {
+  var correct = true;
+  for (var i = 0; i < holder.value.length; i++) {
+    if (holder.value[i] !== word.innerHTML[i]) {
+      correct = false;
+      break;
+    }
   }
-
-  holder.onkeypress = function(){
-      if(holder.value == word.innerHTML){
-          wordsArray.sort(function(){return 0.5 - Math.random()});
-          word.innerHTML = wordsArray[0];
-          points++;
-          holder.value = "";
-      }
+  if (correct) {
+    holder.classList.remove('incorrect');
+    holder.classList.add('correct');
+  } else {
+    holder.classList.remove('correct');
+    holder.classList.add('incorrect');
   }
+}
 
+holder.onkeypress = function() {
+  if (holder.value === word.innerHTML) {
+    updateWord();
+    points++;
+    score.innerHTML = points
+    holder.value = "";
+    holder.classList.remove('incorrect');
+    holder.classList.remove('correct');
+  }
+}
 
-function countDown(){
-    points = 0;
+function updateWord() {
+  wordsArray.sort(function() {
+    return 0.5 - Math.random();
+  });
+  word.innerHTML = wordsArray[0];
+}
+
+function countDown() {
+  var countDownTimer = setInterval(function() {
+    seconds--;
+    timer.innerHTML = seconds;
+
+    if (seconds === 0) {
+      alert("Game over");
+      seconds = 60;
+      points = 0;
+      clearInterval(countDown);
+    }
+  }, 1000);
+}
+
+document.getElementById('restart-button').addEventListener('click', function() {
     seconds = 60;
-    var countDownTimer = setInterval(() => {
-        seconds--;
-        timer.innerHTML = seconds;
-
-        if(seconds === 0){
-            alert("Game over");
-            seconds = 60;
-            points = 0;
-            clearInterval(countDown);
-        }
-
-    }, 1000);
-  }
+    points = 0;
+    score.innerHTML = points
+    holder.value = "";
+    holder.classList.remove('incorrect');
+    holder.classList.remove('correct')
+    updateWord();
+    clearInterval(countDown);
+    countDown();
+  });
